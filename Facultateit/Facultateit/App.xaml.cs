440d8c0a72e1d7682e356749881b6009dbb8bb8c
Wannes Pipien <wannes.pipien@student.pxl.be>
2024-10-20 16:09:21 +0200

    opdracht af

---
+++ Facultateit/Facultateit/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Facultateit
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Facultateit/Facultateit/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Facultateit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int number;
+            bool isValid = int.TryParse(numberTextBox.Text, out number);
+            int result = 1;
+            if (isValid)
+            {
+                do
+                {
+                    result *= number;
+                    number -= 1;
+                } while (number > 0);
+                resultTextBox.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Geen geldig getal");
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            numberTextBox.Text="1";
+            resultTextBox.Clear();
+            numberTextBox.Focus();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
